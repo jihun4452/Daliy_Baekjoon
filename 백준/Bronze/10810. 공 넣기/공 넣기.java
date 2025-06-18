f37e913b0b293bded986ae44fe3ab0a1d1ce8160
ParkJihun <127476112+jihun4452@users.noreply.github.com>
2025-06-18 19:06:14 +0900

    [Bronze III] Title: 공 넣기, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,26 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int N=sc.nextInt();
-		int M = sc.nextInt();
-
-		int[] NumberBucket=new int[N+1];
-
-		for(int i=0; i<M; i++){
-			int first= sc.nextInt();
-			int second=sc.nextInt();
-			int third=sc.nextInt();
-
-			for(int idx= first; idx<=second; idx ++){
-				NumberBucket[idx]=third;
-			}
-		}
-
-		for(int result=1; result<=N; result++ ){
-			System.out.print(NumberBucket[result] + " ");
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[] bucket = new int[N+1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            Arrays.fill(bucket, l, r+1, k);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(bucket[i]).append(' ');
+        }
+        System.out.println(sb);
+    }
 }
