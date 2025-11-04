612475d0e80c546c33d7ad85901a4bb21e803bfe
Parkjihun <jihun6548@naver.com>
2025-11-04 15:19:10 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,45 +1,28 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Stack;
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-		int n = Integer.parseInt(br.readLine());
-		String[] results = new String[n];
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            String line = br.readLine();
+            sb.append(isVPS(line) ? "YES" : "NO").append('\n');
+        }
+        System.out.print(sb);
+    }
 
-		for (int i = 0; i < n; i++) {
-			String s1 = br.readLine();
-			Stack<Character> stack = new Stack<>();
-			String answer = "YES";
-			for (char x : s1.toCharArray()) {
-				if (x == '(') {
-					stack.push(x);
-				} else if (x == ')') {
-					if (stack.isEmpty()) {
-						answer = "NO";
-						break;
-					}
-					stack.pop();
-				}
-			}
-			if(!stack.isEmpty()) {
-				answer = "NO";
-			}
-			results[i] = answer;
-		}
-		for(String result : results) {
-			bw.write(result);
-			bw.newLine();
-		}
+    private static boolean isVPS(String s) {
+        int cnt = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') cnt++;
+            else cnt--;
 
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+            if (cnt < 0) return false;
+        }
+        return cnt == 0;
+    }
 }
